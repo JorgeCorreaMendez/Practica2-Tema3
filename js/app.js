bccb0be4f271322001301865d3c54bf62024df66
Jorge Correa <jorgecorreamen@gmail.com>
2021-02-24 11:38:30 +0000

    Update app.js
    Añadida función para convertir de Fahrenheit a Celius

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
 function IMC() {
-    let kg = window.prompt("Indica el peso en Kilogramos", "kg");
-    let m = window.prompt("Indica la altura en centímetros", "cm");
-  
-    if (kg == "" || m == "") {
-      alert("Error, no se ha introducido ningún valor");
-    } else if (
-      Number.isNaN(Number.parseInt(kg)) ||
-      Number.isNaN(Number.parseInt(m))
-    ) {
-      alert("Error, debe introducir un valor numérico");
-    } else {
-      m = parseFloat(m) / 100;
-      kg = parseInt(kg);
-  
-      let IMC = kg / Math.pow(m, 2);
-      alert(`El índice de masa corporal es: ${IMC}`);
-    }
+  let kg = window.prompt("Indica el peso en Kilogramos", "kg");
+  let m = window.prompt("Indica la altura en centímetros", "cm");
+
+  if (kg == "" || m == "") {
+    alert("Error, no se ha introducido ningún valor");
+  } else if (
+    Number.isNaN(Number.parseInt(kg)) ||
+    Number.isNaN(Number.parseInt(m))
+  ) {
+    alert("Error, debe introducir un valor numérico");
+  } else {
+    m = parseFloat(m) / 100;
+    kg = parseInt(kg);
+
+    let IMC = kg / Math.pow(m, 2);
+    alert(`El índice de masa corporal es: ${IMC}`);
   }
+}
+
+function convertFC() {
+  let fahrenheit = window.prompt("Indica los grados ºF a convertir", "ºF");
+
+  if (fahrenheit == "") {
+    alert("Error, no se ha introducido ningún valor");
+  } else if (Number.isNaN(Number.parseInt(fahrenheit))) {
+    alert("Error, debe introducir un valor numérico");
+  } else {
+    let Celsius = ((parseInt(fahrenheit) - 32) * 5) / 9;
+    alert(`Los grados ${fahrenheit}°F equivalen a ${Celsius}°C`);
+  }
+}
 
 const final = 0;
 let opcion = 1;
@@ -40,13 +53,10 @@ while (opcion != final) {
       IMC();
       break;
     case 2:
-
-        break;
+      convertFC();
+      break;
     default:
       alert("Error, esa opción no esta disponible, vuelva a intentarlo");
       break;
   }
 }
-
-
-
